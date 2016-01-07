bf23091b42c6e01fa1223345e16dc78ed7333b1d
Doug Sisk <doug@fusedinteractive.com>
2016-01-07 10:49:48 -0700

    Testing

---
+++ tests/LaravelBlockReferralSpamTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class LaravelBlockReferralSpamTest extends Orchestra\Testbench\TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.referral_spam_list_location', dirname(__FILE__) . '/../vendor/piwik/referrer-spam-blacklist/spammers.txt');
+
+        $app['router']->get('hello', function () {
+            return 'hello world';
+        });
+
+        $app->make('Illuminate\Contracts\Http\Kernel')->pushMiddleware('DougSisk\BlockReferralSpam\Middleware\BlockReferralSpam');
+    }
+
+    public function testValidRequest()
+    {
+        $this->call('GET', 'hello', [], [], [], [
+            'HTTP_REFERER' => "http://www.google.com"
+        ]);
+
+        $this->assertResponseOk();
+    }
+
+    public function testInvalidRequest()
+    {
+        $this->call('GET', 'hello', [], [], [], [
+            'HTTP_REFERER' => "http://www.allknow.info"
+        ]);
+
+        $this->assertResponseStatus(401);
+    }
+}
