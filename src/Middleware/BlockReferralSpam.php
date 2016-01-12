4a5030fea921d6617aaaeac8fd5a2873bdce117e
Doug Sisk <doug@fusedinteractive.com>
2016-01-12 14:12:04 -0700

    Require referrer

--- src/Middleware/BlockReferralSpam.php
+++ src/Middleware/BlockReferralSpam.php
@@ -25,15 +25,19 @@ class BlockReferralSpam
             }
 
             $referer = utf8_encode($request->headers->get('referer'));
-            preg_match('/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/', $referer, $matches);
-            $fullDomain = $matches[1];
 
-            // Get root domain
-            $domainParts = explode('.', $fullDomain);
-            $rootDomain = $domainParts[count($domainParts) - 2] . '.' . $domainParts[count($domainParts) - 1];
+            // Make sure there's a referrer
+            if (strlen($referer)) {
+                preg_match('/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/', $referer, $matches);
+                $fullDomain = $matches[1];
 
-            if (in_array($fullDomain, $blockedHosts) || in_array($rootDomain, $blockedHosts)) {
-                return response('Spam referral.', 401);
+                // Get root domain
+                $domainParts = explode('.', $fullDomain);
+                $rootDomain = $domainParts[count($domainParts) - 2] . '.' . $domainParts[count($domainParts) - 1];
+
+                if (in_array($fullDomain, $blockedHosts) || in_array($rootDomain, $blockedHosts)) {
+                    return response('Spam referral.', 401);
+                }
             }
         }
 
